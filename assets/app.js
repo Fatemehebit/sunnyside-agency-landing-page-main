0e8f7f29f71522439d2cb542541e2de12185e59b
Fatemehebit <66882790+Fatemehebit@users.noreply.github.com>
2024-08-17 15:51:01 +0330

    Add files via upload

---
+++ assets/app.js
@@ -0,0 +1,7 @@
+
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    const triangle = document.getElementById('act');
+    menu.classList.toggle('active');
+    triangle.classList.toggle('act');
+}
